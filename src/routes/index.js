58ff4e65fb81f813931596a5fd6c51ca2d9fc963
Sherry <shaheryar.rehman@themarketiq.com>
2021-09-17 21:39:33 +0500

    init

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import { Dimensions, Platform, Text, View } from "react-native";
+import { NavigationContainer, getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+
+const routes = () => {
+
+}
+
+export default routes;

a501b28aa0a3a1a2b582c2c8aafe53e70fc5c4de
sajidhassann <sajidhassan1997@gmail.com>
2021-09-19 00:49:01 +0500

    signup route fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ import GettingStarted from '../components/containers/GettingStarted';
 import OnboardEmployee from '../components/containers/OnboardEmployee';
 import AddWorkspace from '../components/containers/AddWorkspace';
 import AddMembers from '../components/containers/AddMembers';
-import Signup from '../components/containers/SignUp';
+import Signup from '../components/containers/Signup';
 import OtpVerification from '../components/containers/OtpVerification';
 
 const Stack = createNativeStackNavigator();

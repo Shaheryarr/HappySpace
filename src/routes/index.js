bd8126d7faf436aa1144f042c196f5038036b865
Sherry <shaheryar.rehman@themarketiq.com>
2021-09-19 12:23:15 +0500

    select a colleague

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ import GettingStarted from '../components/containers/GettingStarted';
 import OnboardEmployee from '../components/containers/OnboardEmployee';
 import AddWorkspace from '../components/containers/AddWorkspace';
 import AddMembers from '../components/containers/AddMembers';
-import Signup from '../components/containers/Signup';
+import Signup from '../components/containers/SignUp';
 import OtpVerification from '../components/containers/OtpVerification';
 import SelectWorkspace from '../components/containers/SelectWorkspace';
 import NewsFeed from '../components/containers/NewsFeed';

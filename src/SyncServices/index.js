d05b3fef0d6a010f30e04b19fa0defeeb5ffbc1d
sajidhassann <sajidhassan1997@gmail.com>
2021-09-19 13:03:48 +0500

    image upload

--- src/SyncServices/index.js
+++ src/SyncServices/index.js
@@ -1,265 +1,295 @@
 import axios from 'axios';
 import {
-	ADD_MEMBERS,
-	ADD_WORKSPACE,
-	CHANGE_PASSWORD_API,
-	CHECK_AUTH_API,
-	GET_POSTS,
-	LOGIN_API,
-	MANAGE_POSTS,
-	OTP_VERIFY_API,
-	REQUEST_PASSWORD_API,
-	RESEND_OTP_API,
-	RESET_PASSWORD_API,
-	SIGNUP_API,
-	UPLOAD_IMAGE_API,
-  	GET_QUIZ_PLAYERS,
+  ADD_MEMBERS,
+  ADD_WORKSPACE,
+  CHANGE_PASSWORD_API,
+  CHECK_AUTH_API,
+  GET_POSTS,
+  LOGIN_API,
+  MANAGE_POSTS,
+  OTP_VERIFY_API,
+  REQUEST_PASSWORD_API,
+  RESEND_OTP_API,
+  RESET_PASSWORD_API,
+  SIGNUP_API,
+  UPLOAD_IMAGE_API,
+  GET_QUIZ_PLAYERS,
+  UPDATE_USER,
 } from './apis';
 
-export const getUserQuizDetails = (workspace_id) => {
-    return new Promise((resolve, reject) => {
-        axios.get(GET_QUIZ_PLAYERS, {
-            params: {
-                workspace_id
-            },
-            withCredentials: true
-        }).then(res => {
-            console.log('getUserQuizDetails res: ', res.data);
-            resolve(res.data);
-        }).catch(err => {
-            console.log('getUserQuizDetails err: ', err.response.data);
-            reject(err);
-        })
-    })
-}
+export const getUserQuizDetails = workspace_id => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(GET_QUIZ_PLAYERS, {
+        params: {
+          workspace_id,
+        },
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('getUserQuizDetails res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('getUserQuizDetails err: ', err.response.data);
+        reject(err);
+      });
+  });
+};
 
 export const getUserAuthentication = () => {
-	return new Promise((resolve, reject) => {
-		axios.get(CHECK_AUTH_API, {
-			withCredentials: true
-		}).then(res => {
-			console.log('getUserAuthentication res: ', res.data);
-			resolve(res.data);
-		}).catch(err => {
-			console.log('getUserAuthentication err: ', err.response.data);
-			reject(err);
-		})
-	})
-}
+  return new Promise((resolve, reject) => {
+    axios
+      .get(CHECK_AUTH_API, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('getUserAuthentication res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('getUserAuthentication err: ', err.response.data);
+        reject(err);
+      });
+  });
+};
 
 export const createPost = params => {
-	return new Promise((resolve, reject) => {
-		axios.put(MANAGE_POSTS, params, {
-			withCredentials: true
-		}).then(res => {
-			console.log('createPost res: ', res.data);
-			resolve(res.data);
-		}).catch(err => {
-			console.log('createPost err: ', err.response.data);
-			reject(err);
-		})
-	})
-}
+  return new Promise((resolve, reject) => {
+    axios
+      .put(MANAGE_POSTS, params, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('createPost res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('createPost err: ', err.response.data);
+        reject(err);
+      });
+  });
+};
 
 export const postImageBase64 = params => {
-	return new Promise((resolve, reject) => {
-		axios.post(UPLOAD_IMAGE_API, params, {
-			withCredentials: true
-		}).then(res => {
-			console.log('postImageBase64 res: ', res.data);
-			resolve(res.data);
-		}).catch(err => {
-			console.log('postImageBase64 err: ', err.response.data);
-			reject(err);
-		})
-	})
-}
+  return new Promise((resolve, reject) => {
+    axios
+      .post(UPLOAD_IMAGE_API, params, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('postImageBase64 res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('postImageBase64 err: ', err.response.data);
+        reject(err);
+      });
+  });
+};
 
 export const postLoginRequest = params => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(LOGIN_API, params, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('postLoginRequest res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('postLoginRequest err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(LOGIN_API, params, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('postLoginRequest res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('postLoginRequest err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const postSignUpRequest = params => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(SIGNUP_API, params, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('postSignUpRequest res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('postSignUpRequest err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(SIGNUP_API, params, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('postSignUpRequest res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('postSignUpRequest err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const postOtpVerify = params => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(OTP_VERIFY_API, params, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('postOtpVerify res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('postOtpVerify err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(OTP_VERIFY_API, params, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('postOtpVerify res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('postOtpVerify err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const changePassword = PARAMS => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(CHANGE_PASSWORD_API, PARAMS, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('changePassword res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('changePassword err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(CHANGE_PASSWORD_API, PARAMS, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('changePassword res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('changePassword err: ', err.response.data);
+        reject(err);
+      });
+  });
+};
+
+export const updateUser = PARAMS => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(UPDATE_USER, PARAMS, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('updateUser res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('updateUser err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const resetPassword = PARAMS => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(RESET_PASSWORD_API, PARAMS, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('resetPassword res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('resetPassword err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(RESET_PASSWORD_API, PARAMS, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('resetPassword res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('resetPassword err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const requestPassword = PARAMS => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(REQUEST_PASSWORD_API, PARAMS, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('requestPassword res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('requestPassword err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(REQUEST_PASSWORD_API, PARAMS, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('requestPassword res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('requestPassword err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const resendOtp = PARAMS => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(RESEND_OTP_API, PARAMS, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('resendOtp res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('resendOtp err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(RESEND_OTP_API, PARAMS, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('resendOtp res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('resendOtp err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const createWorkspace = PARAMS => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(ADD_WORKSPACE, PARAMS, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('createworspace res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('createworspace err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(ADD_WORKSPACE, PARAMS, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('createworspace res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('createworspace err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const addMembers = PARAMS => {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(ADD_MEMBERS, PARAMS, {
-				withCredentials: true,
-			})
-			.then(res => {
-				console.log('addmembers res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('addmembers err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(ADD_MEMBERS, PARAMS, {
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log('addmembers res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('addmembers err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const getPosts = PARAMS => {
-	return new Promise((resolve, reject) => {
-		axios
-			.get(GET_POSTS, {
-				withCredentials: true,
-				params: PARAMS
-			})
-			.then(res => {
-				console.log('getPosts res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('getPosts err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .get(GET_POSTS, {
+        withCredentials: true,
+        params: PARAMS,
+      })
+      .then(res => {
+        console.log('getPosts res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('getPosts err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
 
 export const deletePostById = PARAMS => {
-	return new Promise((resolve, reject) => {
-		axios
-			.delete(MANAGE_POSTS, {
-				withCredentials: true,
-				data: PARAMS
-			})
-			.then(res => {
-				console.log('deletePost res: ', res.data);
-				resolve(res.data);
-			})
-			.catch(err => {
-				console.log('deletePost err: ', err.response.data);
-				reject(err);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .delete(MANAGE_POSTS, {
+        withCredentials: true,
+        data: PARAMS,
+      })
+      .then(res => {
+        console.log('deletePost res: ', res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        console.log('deletePost err: ', err.response.data);
+        reject(err);
+      });
+  });
 };
--- src/components/containers/Profile/index.js
+++ src/components/containers/Profile/index.js
@@ -1,4 +1,4 @@
-import React, {createRef, useRef, useState} from 'react';
+import React, {createRef, useEffect, useRef, useState} from 'react';
 import {
   View,
   Text,
@@ -12,40 +12,78 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import BottomSheet from 'reanimated-bottom-sheet';
 import Animated from 'react-native-reanimated';
-
-// import ImagePicker from 'react-native-image-crop-picker';
+import {useToast} from 'native-base';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import styles from './styles';
 import Buttons from '../../common/Buttons';
 import {connect} from 'react-redux';
-import {handleLogout} from '../../../constants';
+import {handleLogout, isInternetConnected} from '../../../constants';
+import {postImageBase64, updateUser} from '../../../SyncServices';
+import {setUser} from '../../../redux/actions';
+
+const config = {
+  mediaType: 'photo',
+  includeBase64: true,
+  quality: 0.1,
+};
 
 const Profile = ({user, workspace, navigation, dispatch}) => {
-  //Functions Here
-
+  const Toast = useToast();
+  const [image, setImage] = useState(undefined);
   const takePhotoFromCamera = () => {
-    // ImagePicker.openCamera({
-    //   compressImageMaxWidth: 300,
-    //   compressImageMaxHeight: 300,
-    //   cropping: true,
-    //   compressImageQuality: 0.7
-    // }).then(image => {
-    //   console.log(image);
-    //   setImage(image.path);
-    //   this.bs.current.snapTo(1);
-    // });
+    launchCamera(config, res => {
+      const {assets} = res;
+
+      if (assets?.length) {
+        setImage(assets[0]);
+      }
+    });
   };
 
   const choosePhotoFromLibrary = () => {
-    // ImagePicker.openPicker({
-    //   width: 300,
-    //   height: 300,
-    //   cropping: true,
-    //   compressImageQuality: 0.7
-    // }).then(image => {
-    //   console.log(image);
-    //   setImage(image.path);
-    //   this.bs.current.snapTo(1);
-    // });
+    launchImageLibrary(config, res => {
+      const {assets} = res;
+
+      if (assets?.length) {
+        setImage(assets[0]);
+        console.log(assets[0]);
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleSave();
+    return () => {};
+  }, [image]);
+
+  const handleSave = () => {
+    if (image) {
+      let params = {
+        image_name: image.base64,
+      };
+      isInternetConnected()
+        .then(() => {
+          postImageBase64(params).then(res => {
+            const {message: {image_url = ''} = {image_url: ''}} = res;
+            updateUser({image_url})
+              .then(res => {
+                dispatch(setUser({...user, image_url})).then(() => {
+                  Toast.show({title: 'Image uploaded successfully'});
+                });
+              })
+              .catch(() =>
+                Toast.show({
+                  title: 'Error while uploading image',
+                }),
+              );
+          });
+        })
+        .catch(err =>
+          Toast.show({
+            title: 'Please connect to the internet',
+          }),
+        );
+    }
   };
 
   const renderInner = () => (
@@ -131,14 +169,16 @@ const Profile = ({user, workspace, navigation, dispatch}) => {
                   }}>
                   <Icon
                     name="camera"
-                    size={35}
+                    size={30}
                     color="#fff"
                     style={{
-                      opacity: 0.7,
+                      position: 'absolute',
+                      bottom: 0,
+                      right: 2,
                       alignItems: 'center',
                       justifyContent: 'center',
-                      borderWidth: 1,
-                      borderColor: '#fff',
+                      //   borderWidth: 1,
+                      //   borderColor: '#fff',
                       borderRadius: 10,
                     }}
                   />
